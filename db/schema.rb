8d62fef9de28b3984bcc2d3de79aef12254568be
cecile caris <cecilecaris@yahoo.fr>
2016-11-22 11:56:05 -0200

    Add description to Tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122132709) do
+ActiveRecord::Schema.define(version: 20161122133227) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.boolean  "done"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
 end
